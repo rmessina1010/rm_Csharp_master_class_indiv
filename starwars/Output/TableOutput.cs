5ab26add35f246336e029f6cac133a1b9544bd7a
rmessina1010 <rmessina10@gmail.com>
2026-06-08 20:39:31 -0500

    star wars: output table

---
+++ starwars/Output/TableOutput.cs
@@ -0,0 +1,53 @@
+﻿using System.Reflection;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Output.TableOutput{
+    public  class TablePrinter<T>{
+        string TableHeader ;
+        string Divider ;
+        IEnumerable<T> Data;
+        PropertyInfo[] ColumnProperties;
+
+        string[] Whitelist = Array.Empty<string>();
+        public int ColumnWidth = 21;
+            
+            
+            [MemberNotNull(nameof(ColumnProperties), nameof(Divider), nameof(TableHeader))]
+            public void GenerateHeader(){
+            PropertyInfo[] allProperties = typeof(T).GetProperties();
+            ColumnProperties = allProperties
+                .Where(p => Whitelist.Length == 0 || Whitelist.Contains(p.Name))
+                .ToArray();
+
+            var allHeaders = ColumnProperties 
+                .Select(p=>p.Name.PadRight(ColumnWidth));
+            TableHeader = string.Join("|", allHeaders);
+            Divider = new string('_', TableHeader.Length);
+        }
+
+        public void PrintHeader(){
+            Console.WriteLine (TableHeader);
+            Console.WriteLine (Divider);
+        }
+
+        public void PrintBody(){
+            foreach (T row in Data){
+                var allCols = ColumnProperties
+                    .Select( c => (c.GetValue(row)?.ToString() ?? "").PadRight(ColumnWidth));
+                Console.WriteLine(string.Join("|", allCols));
+            }
+        }
+
+        public void PrintTable(){
+            PrintHeader();
+            PrintBody();
+        }
+
+        public TablePrinter (IEnumerable<T> data, string[]? whitelist = null){
+            Data = data;
+            Whitelist = whitelist ?? Array.Empty<string>();
+            GenerateHeader();
+        }
+
+    }
+}
---
+++ starwars/Program.cs
@@ -0,0 +1,34 @@
+﻿using Output.TableOutput;
+
+
+public class Program{
+    public static void Main()
+    {
+        // Creating your test data
+        Starship[] fleet = new Starship[]
+        {
+            new Starship { Name = "X-Wing", Class = "Starfighter", Crew = 1, IsHyperdriveCapable = true },
+            new Starship { Name = "Millennium Falcon", Class = "Light Freighter", Crew = 2, IsHyperdriveCapable = true },
+            new Starship { Name = "TIE Fighter", Class = "Starfighter", Crew = 1, IsHyperdriveCapable = false },
+            new Starship { Name = "Death Star", Class = "Battle Station", Crew = 342953, IsHyperdriveCapable = true }
+        };
+
+        // Test Case 1: Print all columns
+        Console.WriteLine("--- Full Fleet Report ---");
+        var printer = new TablePrinter<Starship>(fleet);
+        printer.PrintTable();
+
+        // Test Case 2: Print only specific columns (Whitelist)
+        Console.WriteLine("\n--- Name and Class Only ---");
+        string[] filter = { "Name", "Class" };
+        var filteredPrinter = new TablePrinter<Starship>(fleet, filter);
+        filteredPrinter.PrintTable();
+    }
+}
+public struct Starship
+{
+    public string Name { get; set; }
+    public string Class { get; set; }
+    public int Crew { get; set; }
+    public bool IsHyperdriveCapable { get; set; }
+}
