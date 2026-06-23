605a32f493a4bbbc6d2eb5da069358640d57a276
rmessina1010 <rmessina10@gmail.com>
2026-06-23 03:20:30 -0500

    cookbook test

--- CookieCookbook/obj/Debug/net6.0/CookieCookbook.AssemblyInfo.cs
+++ CookieCookbook/obj/Debug/net6.0/CookieCookbook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookieCookbook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b429629587869761c6585103ed197b2a53e756d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00cd927209132a695af223424ffae8e0ef75352e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookieCookbook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookieCookbook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
